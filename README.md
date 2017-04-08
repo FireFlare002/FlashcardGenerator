17f5851c8bca5f7cff091639f11f2a1d808a1c24
abhishek0mukherjee <abhishek0mukherjee@gmail.com>
2017-04-08 14:13:19 -0400

    cloze

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FlashcardGenerator
 Flashcards with Cloze Constructer
+
+To run, first run npm install, will install inquirer package.
+
+Then run, npm flashCardApplication.js  , and good luck!
---
+++ questions.js
@@ -0,0 +1,74 @@
+exports.questions = [
+	{
+		full: 'Officially, the longest war in history was between the Netherlands and the Isles of Scilly, which lasted from 1651 to 1986. There were no casualties.',
+		cloze: 'longest'
+	},
+	{
+		full: 'The Anglo-Zanzibar war of 1896 is the shortest war on record lasting an exhausting 38 minutes.',
+		cloze: 'shortest'
+	},
+	{
+		full: 'The largest asteroid ever recorded is a mammoth piece of space rock named Ceres.',
+		cloze: 'Ceres'
+	},
+	{
+		full: 'John F. Kennedy, Anthony Burgess, Aldous Huxley, and C.S. Lewis all died on the same day.',
+		cloze: 'Kennedy'
+	},
+	{
+		full: 'The swastika was actually a symbol of good — until the Nazis got ahold of it.',
+		cloze: 'swastika'
+	},
+	{
+		full: 'Drunk debates were once a key aspect of any important decision-making process.',
+		cloze: 'Drunk'
+	},
+	{
+		full: 'The Olympic Games used to award medals for art.',
+		cloze: 'art'
+	},
+	{
+		full: 'English was once a language for “commoners,” while the British elites spoke French',
+		cloze: 'French'
+	},
+	{
+		full: 'Money was once designed to discourage people from having too much of it',
+		cloze: 'Money'
+	},
+	{
+		full: 'More energy from the sun hits Earth every hour than the planet uses in a year.',
+		cloze: 'year'
+	},
+	{
+		full: 'Tickling may have been used as torture.',
+		cloze: 'Tickling'
+	},
+	{
+		full: 'The first proposal for space travel in English history was made by Oliver Cromwell’s brother-in-law',
+		cloze: 'space'
+	},
+	{
+		full: 'There have been ‘more than 600’ plots against Fidel Castro',
+		cloze: 'Fidel Castro'
+	},
+	{
+		full: 'Neutron stars can spin at a rate of 600 rotations per second.',
+		cloze: 'Neutron'
+	},
+	{
+		full: 'All of space is completely silent.',
+		cloze: 'silent'
+	},
+	{
+		full: 'There is an uncountable number of stars in the known universe.',
+		cloze: 'uncountable'
+	},
+	{
+		full: 'The Apollo astronauts footprints on the moon will probably stay there for at least 100 million years.',
+		cloze: 'footprints'
+	},
+	{
+		full: '99% of our solar systems mass is the sun.',
+		cloze: '99'
+	}
+]
